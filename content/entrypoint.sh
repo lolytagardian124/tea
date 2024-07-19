1213be598b533a23c95ff777c5e85fd7afb82923
wy580477 <98247050+wy580477@users.noreply.github.com>
2024-07-19 10:44:54 +0000

    fix telegram notification on startup

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -22,9 +22,6 @@ SEND_TG_MSG() {
     TELEGRAM_TITLE="$(grep ^telegram-notification-title "${SCRIPT_CONF}" | cut -d= -f2- | sed "s|^[ \t]*||g;s|\r$||")"
     TG_PROXY="$(grep ^telegram-proxy "${SCRIPT_CONF}" | cut -d= -f2- | sed "s|^[ \t]*||g;s|\r$||")"
     if [ "${TELEGRAM_CHAT_ID}" != "" ]; then
-        if [ "${TG_PROXY}" != "" ]; then
-            PROXY_PARAM="-x ${TG_PROXY}"
-        fi
         if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
             msgbody="容器已启动"
         else
@@ -35,7 +32,12 @@ SEND_TG_MSG() {
         msg="$title $timestamp\n$(echo "$msgbody" | sed -e 's|\\|\\\\|g' -e 's|\n|\\n|g' -e 's|\t|\\t|g' -e 's|\"|\\"|g')"
         entities="[{\"offset\":0,\"length\":${#title},\"type\":\"bold\"},{\"offset\":$((${#title} + 1)),\"length\":${#timestamp},\"type\":\"italic\"}]"
         data="{\"chat_id\":\"$TELEGRAM_CHAT_ID\",\"text\":\"$msg\",\"entities\":$entities,\"disable_notification\": true}"
-        curl -s "${PROXY_PARAM}" -o /dev/null -H 'Content-Type: application/json' -X POST -d "$data" https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
+        if [ "${TG_PROXY}" != "" ]; then
+            PROXY_PARAM="-x ${TG_PROXY}"
+            curl -s "${PROXY_PARAM}" -o /dev/null -H 'Content-Type: application/json' -X POST -d "$data" https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
+        else
+            curl -s -o /dev/null -H 'Content-Type: application/json' -X POST -d "$data" https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
+        fi
     fi
 }
 
